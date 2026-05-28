f1e42fe9953a4937ce76743fdde38db1514357a5
devanka761 <devanka@protonmail.com>
2026-05-28 23:36:56 +0700

    fix: update public entry bundles

--- publicEntries.ts
+++ publicEntries.ts
@@ -1,10 +1,10 @@
 const publiEntries = {
   app: "./src/frontend/scripts/app.ts",
-  // home: "./src/frontend/scripts/home.ts",
-  editor: "./src/frontend/scripts/editor.ts"
-  // license: "./src/frontend/scripts/license.ts",
-  // autherror: "./src/frontend/scripts/404.ts",
-  // 404: "./src/frontend/scripts/404.ts"
+  home: "./src/frontend/scripts/home.ts",
+  editor: "./src/frontend/scripts/editor.ts",
+  license: "./src/frontend/scripts/license.ts",
+  autherror: "./src/frontend/scripts/404.ts",
+  404: "./src/frontend/scripts/404.ts"
 }
 
 export default publiEntries
